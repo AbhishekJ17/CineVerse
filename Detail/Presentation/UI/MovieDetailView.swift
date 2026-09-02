73aeac96754b6e6eedf087c4ef81e37133ce5332
Abhishek J <abhishekjadhav007.aj@gmail.com>
2026-09-02 20:36:37 +0530

    Made scroll indicator false

--- Detail/Presentation/UI/MovieDetailView.swift
+++ Detail/Presentation/UI/MovieDetailView.swift
@@ -38,7 +38,7 @@ struct MovieDetailView: View {
                                         Spacer()
                                     }
                                     QuickSpecsRow()
-                                    ScrollView(.horizontal) {
+                                    ScrollView(.horizontal, showsIndicators: false) {
                                         HStack(spacing: 10) {
                                             ForEach(["Action", "Science Fiction", "Drama", "Comedy"], id: \.self) { genre in
                                                 GenrePillView(genreName: genre)
@@ -46,7 +46,7 @@ struct MovieDetailView: View {
                                         }
                                     }
                                     HeroCardTitle(title: "Production Logos Shelf")
-                                    ScrollView(.horizontal) {
+                                    ScrollView(.horizontal, showsIndicators: false) {
                                         HStack(spacing: 10) {
                                             ForEach(["/71BqEFAF4V3qjjMPCpLuyJFB9A.png", "/nw4kyc29QRpNtFbdsBHkRSFavvt.png",
                                                      "/hUzeosd33nzE5MCNsZxCGEKTXaQ.png", "/qx9K6bFWJupwde0xQDwOvXkOaL8.png"], id: \.self) { imagePath in
